9cdea65b7110a0b301d58feb18c20ff962dda423
Mike Walmsley <walmsleymk1@gmail.com>
2022-05-14 14:31:44 +0100

    split into chunks

--- notebooks/legs_low_z_rsync.py
+++ notebooks/legs_low_z_rsync.py
@@ -1,3 +1,4 @@
+import chunk
 import pandas as pd
 
 
@@ -18,11 +19,26 @@ if __name__ == '__main__':
     all_files = list(df['file_loc'])
     print(all_files[:3])
 
-    all_files_path = '/nvme1/scratch/walml/repos/pytorch-galaxy-datasets/notebooks/temp_legs_z_below_0p1_all_files.txt'
+    all_files_base_path = '/nvme1/scratch/walml/repos/pytorch-galaxy-datasets/notebooks/temp_legs_z_below_0p1_all_files'
 
-    with open(all_files_path, 'w') as f:
-        f.write('\n'.join(all_files))
+    # rsync is really slow, probably super-linearly, at building file list for large transfers
+    # split into many chunks instead
+    chunk_size = 50000
+    file_n = 0
+    chunk_paths = []
+    while file_n < len(all_files):
+        this_chunk_loc = all_files_base_path + '_{}.txt'.format(file_n)
 
-    print(f'rsync --dry-run --files-from {all_files_path} walml@galahad.ast.man.ac.uk:/share/nas2/walml/galaxy_zoo/decals/dr8/jpg  /home/walml/repos/pytorch-galaxy-datasets/derived_data/legs_z_below_0p1_to_upload')
+        these_files = all_files[file_n:file_n+chunk_size]
 
-    print(f'rsync -v --files-from {all_files_path} walml@galahad.ast.man.ac.uk:/share/nas2/walml/galaxy_zoo/decals/dr8/jpg  /home/walml/repos/pytorch-galaxy-datasets/derived_data/legs_z_below_0p1_to_upload')
+        with open(this_chunk_loc, 'w') as f:
+            f.write('\n'.join(these_files))
+
+        chunk_paths.append(this_chunk_loc)
+        file_n += chunk_size
+
+    print(' '.join(chunk_paths))
+
+    # print(f'rsync --dry-run --files-from {all_files_path} walml@galahad.ast.man.ac.uk:/share/nas2/walml/galaxy_zoo/decals/dr8/jpg  /home/walml/repos/pytorch-galaxy-datasets/derived_data/legs_z_below_0p1_to_upload')
+
+    print(f'rsync -v --files-from {chunk_paths[0]} walml@galahad.ast.man.ac.uk:/share/nas2/walml/galaxy_zoo/decals/dr8/jpg  /home/walml/repos/pytorch-galaxy-datasets/derived_data/legs_z_below_0p1_to_upload')
