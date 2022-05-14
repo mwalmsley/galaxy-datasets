8c8c9caf8694c74b480c8076e1e65b6edfc48681
Mike Walmsley <walmsleymk1@gmail.com>
2022-05-14 06:32:01 +0100

    tweak path

--- notebooks/legs_low_z_rsync.py
+++ notebooks/legs_low_z_rsync.py
@@ -4,7 +4,7 @@ import pandas as pd
 if __name__ == '__main__':
 
     # note - the low z (z<0.1) catalog!
-    df = pd.read_parquet('/home/walml/repos/decals-rings/results/rings_megacatalog_low_z.parquet', columns=['dr8_id', 'brickid', 'objid', 'iauname', 'redshift'])
+    df = pd.read_parquet('/home/walml/repos/decals-rings/results/rings_megacatalog_z_below_0p1.parquet', columns=['dr8_id', 'brickid', 'objid', 'iauname', 'redshift'])
 
 
     df['brickid'] = df['id_str'].apply(lambda x: x.split('_')[0])
@@ -17,9 +17,9 @@ if __name__ == '__main__':
     all_files = list(df['file_loc'])
     print(all_files[:3])
 
-    all_files_path = '/nvme1/scratch/walml/repos/pytorch-galaxy-datasets/notebooks/temp_legs_low_z_all_files.txt'
+    all_files_path = '/nvme1/scratch/walml/repos/pytorch-galaxy-datasets/notebooks/temp_legs_z_below_0p1_all_files.txt'
 
     with open(all_files_path, 'w') as f:
         f.write('\n'.join(all_files))
 
-    print(f'rsync --dry-run --files-from {all_files_path} walml@galahad.ast.man.ac.uk:/share/nas2/walml/galaxy_zoo/decals/dr8/jpg  /home/walml/repos/pytorch-galaxy-datasets/derived_data/legs_low_z_to_upload')
+    print(f'rsync --dry-run --files-from {all_files_path} walml@galahad.ast.man.ac.uk:/share/nas2/walml/galaxy_zoo/decals/dr8/jpg  /home/walml/repos/pytorch-galaxy-datasets/derived_data/legs_z_below_0p1_to_upload')
