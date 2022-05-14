718fb2be5c2efdc4d26716e9096d574859543384
Mike Walmsley <walmsleymk1@gmail.com>
2022-05-14 06:30:01 +0100

    add low z rsync attempt

---
+++ notebooks/legs_low_z_rsync.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+if __name__ == '__main__':
+
+    # note - the low z (z<0.1) catalog!
+    df = pd.read_parquet('/home/walml/repos/decals-rings/results/rings_megacatalog_low_z.parquet', columns=['dr8_id', 'brickid', 'objid', 'iauname', 'redshift'])
+
+
+    df['brickid'] = df['id_str'].apply(lambda x: x.split('_')[0])
+    df['objid'] = df['id_str'].apply(lambda x: x.split('_')[1])
+    # relative to rsync root
+    df['file_loc'] = df['brickid'] + '/' + df['id_str'] + '.jpg'
+
+    print(df['file_loc'][0])
+
+    all_files = list(df['file_loc'])
+    print(all_files[:3])
+
+    all_files_path = '/nvme1/scratch/walml/repos/pytorch-galaxy-datasets/notebooks/temp_legs_low_z_all_files.txt'
+
+    with open(all_files_path, 'w') as f:
+        f.write('\n'.join(all_files))
+
+    print(f'rsync --dry-run --files-from {all_files_path} walml@galahad.ast.man.ac.uk:/share/nas2/walml/galaxy_zoo/decals/dr8/jpg  /home/walml/repos/pytorch-galaxy-datasets/derived_data/legs_low_z_to_upload')
