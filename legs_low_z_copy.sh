9d8e192a49d00da4f701cfeb30aad9cab1da55d8
Mike Walmsley <walmsleymk1@gmail.com>
2022-05-15 10:49:14 +0100

    try /./, works interactively

--- legs_low_z_copy.sh
+++ legs_low_z_copy.sh
@@ -15,4 +15,4 @@ pwd; hostname; date
 
 head -n 1 /share/nas2/walml/repos/pytorch-galaxy-datasets/notebooks/temp_legs_z_below_0p1_all_files.txt | xargs -I{} -n 1 -P 1 echo /share/nas2/walml/galaxy_zoo/decals/dr8/jpg/{} /share/nas2/walml/galaxy_zoo/decals/dr8/low_z_jpg/{}
 
-head -n 1 /share/nas2/walml/repos/pytorch-galaxy-datasets/notebooks/temp_legs_z_below_0p1_all_files.txt | xargs -I{} -n 1 -P 1 rsync -avR /share/nas2/walml/galaxy_zoo/decals/dr8/jpg/{} low_z_jpg/{}
+head -n 1 /share/nas2/walml/repos/pytorch-galaxy-datasets/notebooks/temp_legs_z_below_0p1_all_files.txt | xargs -I{} -n 1 -P 1 rsync --relative /share/nas2/walml/galaxy_zoo/decals/dr8/jpg/./{} /share/nas2/walml/galaxy_zoo/decals/dr8/low_z_jpg/{}
