367f244780665d04058fd795202580af42d7f4ad
Mike Walmsley <walmsleymk1@gmail.com>
2022-05-15 10:52:26 +0100

    try without {}

--- legs_low_z_copy.sh
+++ legs_low_z_copy.sh
@@ -13,6 +13,8 @@ pwd; hostname; date
 
 # rsync -R -a /share/nas2/walml/galaxy_zoo/decals/dr8/jpg/397101/397101_4203.jpg /share/nas2/walml/galaxy_zoo/decals/dr8/low_z_jpg
 
-head -n 1 /share/nas2/walml/repos/pytorch-galaxy-datasets/notebooks/temp_legs_z_below_0p1_all_files.txt | xargs -I{} -n 1 -P 1 echo /share/nas2/walml/galaxy_zoo/decals/dr8/jpg/{} /share/nas2/walml/galaxy_zoo/decals/dr8/low_z_jpg/{}
+head -n 1 /share/nas2/walml/repos/pytorch-galaxy-datasets/notebooks/temp_legs_z_below_0p1_all_files.txt | rsync --relative /share/nas2/walml/galaxy_zoo/decals/dr8/jpg/./{} /share/nas2/walml/galaxy_zoo/decals/dr8/low_z_jpg
 
-head -n 1 /share/nas2/walml/repos/pytorch-galaxy-datasets/notebooks/temp_legs_z_below_0p1_all_files.txt | xargs -I{} -n 1 -P 1 rsync --relative /share/nas2/walml/galaxy_zoo/decals/dr8/jpg/./{} /share/nas2/walml/galaxy_zoo/decals/dr8/low_z_jpg/{}
+# head -n 1 /share/nas2/walml/repos/pytorch-galaxy-datasets/notebooks/temp_legs_z_below_0p1_all_files.txt | xargs -I{} -n 1 -P 1 echo /share/nas2/walml/galaxy_zoo/decals/dr8/jpg/{} /share/nas2/walml/galaxy_zoo/decals/dr8/low_z_jpg/{}
+
+head -n 1 /share/nas2/walml/repos/pytorch-galaxy-datasets/notebooks/temp_legs_z_below_0p1_all_files.txt | xargs -I{} -n 1 -P 1 rsync --relative /share/nas2/walml/galaxy_zoo/decals/dr8/jpg/./{} /share/nas2/walml/galaxy_zoo/decals/dr8/low_z_jpg
