f01085df48b1e9b591db35d2fcaaec13564766fa
Ben Aussel <baussel@yahoo.de>
2022-12-12 14:06:55 +0100

    Adding links to Euclid data

--- galaxy_datasets/shared/gz_euclid.py
+++ galaxy_datasets/shared/gz_euclid.py
@@ -10,13 +10,13 @@ from galaxy_datasets.shared import download_utils
 
 
 def gz_hubble(root, train, download):
-    logging.info('Setting up gz_hubble dataset')
+    logging.info('Setting up gz_euclid dataset')
     resources = [
-        ('https://dl.dropboxusercontent.com/s/xnktj9hq6xig0a7/hubble_ortho_train_catalog.parquet', 'c6cb821f7ebefb583dc74488cf7bfc5f'),  # train catalog
-        ('https://dl.dropboxusercontent.com/s/1g9lwih9944sys8/hubble_ortho_test_catalog.parquet', '05e01ed822b34400f32977280eebec87'),  # test catalog
-        ('https://dl.dropboxusercontent.com/s/k9xco1mtp8bw60v/hubble_images.tar.gz', None)  # the images 'b6fed2463bb2d17ddb8302f6b060534a'
+        ('https://drive.google.com/file/d/1Vm__dpPbyojLFi58RTTZoyzmFSWv9Aup/view?usp=share_link', None),  # train catalog
+        ('https://drive.google.com/file/d/1Vm__dpPbyojLFi58RTTZoyzmFSWv9Aup/view?usp=share_link',None),  # test catalog
+        ('https://drive.google.com/file/d/1-6mwYz0Aa3Demd594dWr_1Gi3YTCkoIa/view?usp=share_link', None)  # the images 'b6fed2463bb2d17ddb8302f6b060534a'
     ]
-    images_to_spotcheck = ['10000325.jpg', '20163083.jpg']
+    images_to_spotcheck = ['20163083.jpg']
 
     downloader = download_utils.DatasetDownloader(root, resources, images_to_spotcheck)
     if download is True:
@@ -27,15 +27,15 @@ def gz_hubble(root, train, download):
 
     useful_columns = label_cols + ['filename']
     if train:
-        train_catalog_loc = os.path.join(root, 'hubble_ortho_train_catalog.parquet')
+        train_catalog_loc = os.path.join(root, 'catalog_hubble_bright.csv')
         catalog = pd.read_parquet(train_catalog_loc, columns=useful_columns)
     else:
-        test_catalog_loc = os.path.join(root, 'hubble_ortho_test_catalog.parquet')
+        test_catalog_loc = os.path.join(root,  'catalog_hubble_bright.csv')
         catalog = pd.read_parquet(test_catalog_loc, columns=useful_columns)
 
     catalog['file_loc'] = catalog.apply(lambda x: os.path.join(downloader.image_dir, x['filename']), axis=1)  # no subfolder
 
-    logging.info('gz_hubble dataset ready')
+    logging.info('gz_euclid dataset ready')
     return catalog, label_cols
 
 # TODO may change features to featured-or-disk
