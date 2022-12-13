b1f916b93f0934a78f9b75120a436b2ef754d2f9
Mike Walmsley <walmsleymk1@gmail.com>
2022-12-13 16:58:37 +0000

    update checksums/url

--- galaxy_datasets/shared/gz2.py
+++ galaxy_datasets/shared/gz2.py
@@ -14,7 +14,7 @@ def gz2(root, train, download):
     resources = [
         ('https://dl.dropboxusercontent.com/s/vu77e3sh2s5c250/gz2_train_catalog.parquet', '4601cf8f7bea8ab0d79b2aada1a4663d'),  # the train catalog
         ('https://dl.dropboxusercontent.com/s/t8eh6f3oupndpl3/gz2_test_catalog.parquet', '65bc4d26cb1dd1426fed4f3c357cb42c'),  # the test catalog
-        ('https://zenodo.org/record/3565489/files/images_gz2.zip', '4c085a3d2a30915f4daef365175f509d')  # the images
+        ('https://dl.dropboxusercontent.com/s/5so7yof2afe761p/images_gz2.tar.gz', 'e3eab2fec57a6a60577236b9e0a6913d')  # the images
     ]
     images_to_spotcheck = ['587722/587722981741363294.jpg']
 
