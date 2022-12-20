ad1857084f03f465e648a9ad6e0e809bbd86d448
Mike Walmsley <walmsleymk1@gmail.com>
2022-12-20 14:18:14 +0000

    missed an import

--- galaxy_datasets/__init__.py
+++ galaxy_datasets/__init__.py
@@ -2,6 +2,7 @@ from galaxy_datasets.shared.gz_candels import gz_candels
 from galaxy_datasets.shared.gz_decals_5 import gz_decals_5
 from galaxy_datasets.shared.gz_hubble import gz_hubble
 from galaxy_datasets.shared.gz2 import gz2
+from galaxy_datasets.shared.demo_rings import demo_rings
 from galaxy_datasets.shared.tidal import tidal
 
 from galaxy_datasets.check_internal_urls import INTERNAL_URLS_EXIST
