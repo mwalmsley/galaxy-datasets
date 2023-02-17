b5c547b0edf36097d3accb1686a43f67561ceaf9
Mike Walmsley <walmsleymk1@gmail.com>
2023-02-17 11:41:03 +0000

    add release button, bump to 0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="galaxy-datasets",
-    version="0.0.5",
+    version="0.0.6",
     author="Mike Walmsley",
     author_email="walmsleymk1@gmail.com",
     description="Galaxy Zoo datasets for PyTorch/TensorFlow",
