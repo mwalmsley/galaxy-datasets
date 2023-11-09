672cc13ca2eaad6dba70ffdea65382809dc77507
Mike Walmsley <walmsleymk1@gmail.com>
2023-11-09 16:57:22 -0500

    rename

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="galaxy-datasets",
+    name="galaxy_datasets",
     version="0.0.15",
     author="Mike Walmsley",
     author_email="walmsleymk1@gmail.com",
