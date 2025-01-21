301110b03ad12bcbe89318fb6799b3276f45b054
Mike Walmsley <walmsleymk1@gmail.com>
2025-01-21 10:10:33 -0500

    prep for albumentations 2.0

--- galaxy_datasets/transforms.py
+++ galaxy_datasets/transforms.py
@@ -202,7 +202,7 @@ def default_transforms(
             A.CenterCrop(
                 height=initial_center_crop,  # initial crop
                 width=initial_center_crop,
-                always_apply=True
+                # always_apply=True
             )
         ]
 
@@ -212,12 +212,11 @@ def default_transforms(
         A.Rotate(limit=180, interpolation=1,
                     always_apply=True, border_mode=0, value=0),
         A.RandomResizedCrop(
-            height=resize_after_crop,  # after crop resize
-            width=resize_after_crop,
+            size=(resize_after_crop, resize_after_crop), # after crop resize
             scale=crop_scale_bounds,  # crop factor
             ratio=crop_ratio_bounds,  # crop aspect ratio
             interpolation=1,  # This is "INTER_LINEAR" == BILINEAR interpolation. See: https://docs.opencv.org/3.4/da/d54/group__imgproc__transform.html
-            always_apply=True
+            # always_apply=True
         ),  # new aspect ratio
         A.VerticalFlip(p=0.5)
     ]
@@ -275,12 +274,12 @@ def base_transforms(pytorch_greyscale):
     if pytorch_greyscale:
         return [
             A.Lambda(
-                name="ToGray", image=ToGray(reduce_channels=True), always_apply=True
+                name="ToGray", image=ToGray(reduce_channels=True) #, always_apply=True
             )
         ]
     else:
        return [
-            A.Lambda(name="RemoveAlpha", image=RemoveAlpha(), always_apply=True)
+            A.Lambda(name="RemoveAlpha", image=RemoveAlpha()) #, always_apply=True)
         ]
         
 
