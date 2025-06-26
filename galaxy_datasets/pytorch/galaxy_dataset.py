c557b8e925761cd4ac9ba9bcbe87526d8c732d70
Mike Walmsley <walmsleymk1@gmail.com>
2025-06-26 12:12:20 -0400

    old

--- galaxy_datasets/pytorch/galaxy_dataset.py
+++ galaxy_datasets/pytorch/galaxy_dataset.py
@@ -58,7 +58,8 @@ class CatalogDataset(torch_Dataset):
 
         self.label_cols = label_cols
         self.transform = transform
-        self.target_transform = target_transform
+        # self.target_transform = target_transform
+        assert target_transform is None, "target_transform not supported in this dataset"
 
     def __len__(self) -> int:
         return len(self.catalog)
@@ -88,76 +89,78 @@ class CatalogDataset(torch_Dataset):
         result["image"] = image
 
         if self.label_cols is not None:
-            # load the labels. If no self.label_cols, will
-            label = get_galaxy_label(galaxy, self.label_cols)
+            for col in self.label_cols:
+                # add each label column to the result dict
+                # this is useful for multi-label classification
+                result[col] = galaxy[col]
 
-            if self.target_transform:
-                label = self.target_transform(label)
-
-            result["label"] = label
-
-        return result
+            # deprecated for simplicity
+            # if self.target_transform:
+            #     label = self.target_transform(label)
 
 
+        return result  # dict with keys 'id_str', 'image', and then each key in label_cols if label_cols is not None
+
+# not used, instead use HuggingFace directly as a dataset
 # https://huggingface.co/docs/datasets/en/use_with_pytorch
-# https://huggingface.co/docs/datasets/en/image_process
-class HF_GalaxyDataset(torch_Dataset):
-    def __init__(
-        self,
-        dataset,#: hf_datasets.Dataset,  # HF Dataset
-        label_cols=['label'],  # assumed to be columns in the dataset which can be stacked in order
-        transform=None,
-        target_transform=None,
-    ):
-        """
-        Create custom PyTorch Dataset using HuggingFace dataset
+# # https://huggingface.co/docs/datasets/en/image_process
+# class HF_GalaxyDataset(torch_Dataset):
+#     def __init__(
+#         self,
+#         dataset,#: hf_datasets.Dataset,  # HF Dataset
+#         label_cols=['label'],  # assumed to be columns in the dataset which can be stacked in order
+#         transform=None,
+#         target_transform=None,
+#     ):
+#         """
+#         Create custom PyTorch Dataset using HuggingFace dataset
 
 
-        Args:
-            name (str): Name of HF dataset
-            transform (callable, optional): See Pytorch Datasets. Defaults to None.
-            target_transform (callable, optional): See Pytorch Datasets. Defaults to None.
-        """
-        # https://huggingface.co/docs/datasets/en/use_with_pytorch
-        self.dataset = dataset.with_format("torch")
-        self.label_cols = label_cols
-        self.transform = transform
-        self.target_transform = target_transform
+#         Args:
+#             name (str): Name of HF dataset
+#             transform (callable, optional): See Pytorch Datasets. Defaults to None.
+#             target_transform (callable, optional): See Pytorch Datasets. Defaults to None.
+#         """
+#         # https://huggingface.co/docs/datasets/en/use_with_pytorch
+#         self.dataset = dataset.with_format("torch")
+#         self.label_cols = label_cols
+#         self.transform = transform
+#         self.target_transform = target_transform
 
-    def __len__(self) -> int:
-        return len(self.dataset)
+#     def __len__(self) -> int:
+#         return len(self.dataset)
 
-    def __getitem__(self, idx: int):
-        example:dict = self.dataset[idx]
+#     def __getitem__(self, idx: int):
+#         example:dict = self.dataset[idx]
         
-        if self.transform:
-             example['image'] = self.transform(example['image'])
+#         if self.transform:
+#              example['image'] = self.transform(example['image'])
 
-        if self.target_transform:
-            example = self.target_transform(example)  # slightly generalised: target_transform to expect and yield example, changing the targets (labels)
+#         if self.target_transform:
+#             example = self.target_transform(example)  # slightly generalised: target_transform to expect and yield example, changing the targets (labels)
 
-        if self.label_cols is None: 
-            return example['image']  # predict mode
-        else:
-            return (example['image'], torch.stack([example[label] for label in self.label_cols]).squeeze())
-        # ultimately I would prefer to migrate to 
-        # return example  # dict like {'image': image, 'label_a': label_a, ...}
+#         if self.label_cols is None: 
+#             return example['image']  # predict mode
+#         else:
+#             return (example['image'], torch.stack([example[label] for label in self.label_cols]).squeeze())
+#         # ultimately I would prefer to migrate to 
+#         # return example  # dict like {'image': image, 'label_a': label_a, ...}
 
-    def get_catalogs(self) -> dict[pd.DataFrame]:
-        # return the original dataset
-        dataset = self.dataset.copy()
+#     def get_catalogs(self) -> dict[pd.DataFrame]:
+#         # return the original dataset
+#         dataset = self.dataset.copy()
 
-        # has splits
-        if isinstance(dataset, hf_datasets.DatasetDict) or isinstance(dataset, dict):
-            for key in dataset.keys():
-                dataset_pd = dataset[key].with_format(type="pandas")
-                dataset[key] = dataset_pd[:]
-        else:  # single split
-            print(type(dataset))
-            dataset_pd = dataset.with_format(type="pandas")
-            dataset = dataset_pd[:]
+#         # has splits
+#         if isinstance(dataset, hf_datasets.DatasetDict) or isinstance(dataset, dict):
+#             for key in dataset.keys():
+#                 dataset_pd = dataset[key].with_format(type="pandas")
+#                 dataset[key] = dataset_pd[:]
+#         else:  # single split
+#             print(type(dataset))
+#             dataset_pd = dataset.with_format(type="pandas")
+#             dataset = dataset_pd[:]
     
-        return dataset
+#         return dataset
 
 
 
@@ -207,11 +210,11 @@ def load_fits_file(loc):
     return np.expand_dims(x, axis=2)  # HWC
 
 
-def get_galaxy_label(galaxy: pd.Series, label_cols: List) -> np.ndarray:
-    # pytorch 1.12 is happy with float32 for both dirichlet and cross-entropy
-    return (
-        galaxy[label_cols].astype(np.float32).values.squeeze()
-    )  # squeeze for if there's one label_col
+# def get_galaxy_label(galaxy: pd.Series, label_cols: List) -> np.ndarray:
+#     # pytorch 1.12 is happy with float32 for both dirichlet and cross-entropy
+#     return (
+#         galaxy[label_cols].astype(np.float32).values.squeeze()
+#     )  # squeeze for if there's one label_col
 
 
 if __name__ == "__main__":
